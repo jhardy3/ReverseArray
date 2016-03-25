1d596d6fef8488dcae2cfd0856f84eb9d2bf6358
Jake Hardy <jhardy3@live.com>
2016-03-25 09:04:48 -0600

    Flippen Finished

--- ReverseArray.playground/Contents.swift
+++ ReverseArray.playground/Contents.swift
@@ -3,3 +3,16 @@
 import UIKit
 
 // Create a function that takes an array of integers and prints the array in reverse
+
+
+func printThisFlippenArray<T>(array: [T]) -> [T] {
+    var returnArray = [T]()
+    
+    for item in array {
+        returnArray.insert(item, atIndex: 0)
+    }
+    
+    return returnArray
+}
+
+printThisFlippenArray([1, 2, 3])
